69549b895416b30eaa1584b3dfcfb3efd4bc1f99
joseangel_sc <jasc@joseangelsc.com>
2018-01-18 12:47:40 -0600

    lets see if password works this way

---
+++ ftpserver.sh
@@ -0,0 +1,12 @@
+sudo apt-get install pure-ftpd
+sudo groupadd ftpgroup
+sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d /dev/null
+sudo mkdir /home/pi/FTP
+sudo chown -R ftpuser:ftpgroup /home/pi/FTP
+raspberry
+raspberry
+sudo pure-pw useradd upload -u ftpuser -g ftpgroup -d /home/pi/FTP -m
+raspberry
+sudo pure-pw mkdb
+sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb
+sudo service pure-ftpd restart
